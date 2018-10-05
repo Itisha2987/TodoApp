638b96285d5212348458e356648b1a0ce571db4a
itisha2987 <itishapatidar1@gmail.com>
2018-10-05 10:17:43 +0530

    routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
- get 'todos/index'
- get 'todos/destroy'
-  get 'todos/edit'
- get 'todos/new'
- get 'todos/create'
-  post 'todos/new'
-  post 'todos/index'
-  post 'todos/edit'
-  get 'todos/update'
+ resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
